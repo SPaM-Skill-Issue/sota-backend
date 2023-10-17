695631d454e967cb526a28eb1b547fe19184364b
Hamiz5401 <kittiporn132@gmail.com>
2023-10-17 19:28:14 +0700

    add codes for 'gen_new_key' function

--- routers/apikeygen_router.py
+++ routers/apikeygen_router.py
@@ -1,19 +1,21 @@
-from fastapi import APIRouter
-from database_connection import keys_collection
+from fastapi import APIRouter, HTTPException
+from ..database_connection import keys_collection
 from pydantic import BaseModel
+import random, string
 
 router = APIRouter(
     prefix="/apikeygen",
     tags=["keygen"]
 )
 
+
 class ScopeEnum(BaseModel):
     PUBLISH_MEDAL: bool
     PUBLISH_AUDIENCE: bool
 
+
 @router.post("/")
 def gen_new_key(scope: ScopeEnum):
-
     """
     Generate 20 character long key,
     check permissions via payload (body),
@@ -27,4 +29,16 @@ def gen_new_key(scope: ScopeEnum):
         }
     }
     """
-    pass
+    if scope.PUBLISH_MEDAL or scope.PUBLISH_AUDIENCE is None:
+        return HTTPException(400, detail="Permission should not be null")
+
+    key = ''.join(random.choices(string.ascii_letters + string.digits, k=20))  # alpha-numeric
+    keys_collection.insert_one(
+        {
+            "key": key,
+            "scope": {
+                "PUBLISH_MEDAL": scope.PUBLISH_MEDAL,
+                "PUBLISH_AUDIENCE": scope.PUBLISH_AUDIENCE
+            }
+        }
+    )
