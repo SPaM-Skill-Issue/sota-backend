a1c9eb2653045604a9fb54481534b8c6f6a15562
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-10-24 08:55:19 +0700

    feat: implement endpoint GET /sports/

--- routers/sports_router.py
+++ routers/sports_router.py
@@ -1,12 +1,29 @@
 from fastapi import APIRouter
+from database_connection import sport_detail_collection
+
+router = APIRouter(prefix="/sports", tags=["sports"])
 
 
-router = APIRouter(
-    prefix="/sports",
-    tags=["sports"]
-)
-
-
-@router.get("/sports")
+@router.get("/")
 def get_all_sports_id():
-    return {"test": "all good"}
+
+    sport_pairs = sport_detail_collection.aggregate(
+        [
+            {
+                "$replaceRoot": {
+                    "newRoot": {
+                        "$arrayToObject": [
+                            [{"k": {"$toString": "$sport_id"}, "v": "$sport_name"}]
+                        ]
+                    }
+                }
+            }
+        ]
+    )
+
+    res = dict()
+    for entry in sport_pairs:
+        item = list(entry.items())[0]
+        res[item[0]] = item[1]
+
+    return res
