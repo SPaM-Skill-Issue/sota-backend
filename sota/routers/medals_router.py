cb0de0866f9a6b44539695bd0d5cd6b1f8883159
Supakrit Aphonmaeklong <supakritaphonmaeklong@gmail.com>
2023-10-28 15:00:48 +0700

    feat: Implement GET /medals endpoint

--- sota/routers/medals_router.py
+++ sota/routers/medals_router.py
@@ -1,12 +1,39 @@
 from fastapi import APIRouter
+from ..database_connection import medal_collection
 
 
-router = APIRouter(
-    prefix="/medals",
-    tags=["medals"]
-)
+router = APIRouter(prefix="/medals", tags=["medals"])
 
 
 @router.get("/")
 def get_medals():
-    return {"test": "all good"}
+    pipeline = [
+        {"$unwind": {"path": "$sports"}},
+        {
+            "$group": {
+                "_id": "$country_code",
+                "gold": {"$sum": "$sports.gold"},
+                "silver": {"$sum": "$sports.silver"},
+                "bronze": {"$sum": "$sports.bronze"},
+            }
+        },
+        {
+            "$group": {
+                "_id": None,
+                "data": {
+                    "$push": {
+                        "k": "$_id",
+                        "v": {
+                            "gold": "$gold",
+                            "silver": "$silver",
+                            "bronze": "$bronze",
+                        },
+                    }
+                },
+            }
+        },
+        {"$replaceRoot": {"newRoot": {"$arrayToObject": "$data"}}},
+    ]
+
+    medals = list(medal_collection.aggregate(pipeline))
+    return medals[0] if medals else {}
