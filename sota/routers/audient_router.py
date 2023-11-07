9dcaa57f2f528050a95fe065ae111d4beedd2566
Supakrit Aphonmaeklong <supakritaphonmaeklong@gmail.com>
2023-11-07 16:12:29 +0700

    change 422 to 400 using fast api exception handler, and more validations on base model

--- sota/routers/audient_router.py
+++ sota/routers/audient_router.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, Depends
-from pymongo import DESCENDING
-from pydantic import BaseModel, validator
+# Standard library imports
 from typing import List
-from ..database_connection import audient_collection
+
+# Third-party imports
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, validator
+import pycountry
+
+# Local application imports
+from ..database_connection import audient_collection, sport_detail_collection
 from .deps.auth_deps import check_auth_key, CheckPermissionsOfKey, AuthScope
 
 
-router = APIRouter(
-    prefix="/audient",
-    tags=["audient"]
-)
+country_codes = [country.alpha_2 for country in pycountry.countries]
+router = APIRouter(prefix="/audient", tags=["audient"])
+
 
 class RequestAudientData(BaseModel):
     id: str
@@ -22,37 +26,52 @@ class RequestAudientData(BaseModel):
     def validate_gender(cls, value):
         allowed_values = {"M", "F", "N"}
         if value not in allowed_values:
-            raise ValueError("Invalid value for gender. It must be 'M', 'F', or 'N'.")
+            raise ValueError(
+                f"Invalid value for gender {value}. It must be 'M', 'F', or 'N'."
+            )
         return value
+
+    @validator("country_code")
+    def validate_country_code(cls, value):
+        if value not in country_codes:
+            raise ValueError(f"Country {value} doesn't exist")
+        return value
+
+    @validator("sport_id", each_item=True)
+    def validate_sport_id(cls, value):
+        if not sport_detail_collection.find_one({"sport_id": value}):
+            raise ValueError(f"The sport_id {value} doesn't exist")
+        return value
+
 
 class RequestListOfAudientData(BaseModel):
     audience: List[RequestAudientData]
 
+
 def convert_data(data):
-    return{
+    return {
         "country_code": data["country_code"],
         "sport_id": data["sport_id"],
         "gender": data["gender"],
-        "age": data["age"]
+        "age": data["age"],
     }
+
 
 @router.get("/")
 def get_audient():
     audient_all = [convert_data(audient) for audient in audient_collection.find()]
     return audient_all
 
-@router.post("/update_audient_info", dependencies=[
-    Depends(check_auth_key),
-    Depends(CheckPermissionsOfKey([
-        AuthScope.PUBLISH_AUDIENCE
-    ]))
-])
+
+@router.post(
+    "/update_audient_info",
+    dependencies=[
+        Depends(check_auth_key),
+        Depends(CheckPermissionsOfKey([AuthScope.PUBLISH_AUDIENCE])),
+    ],
+)
 async def update_audient_info(data: RequestListOfAudientData):
-    audient_data = data.dict()
+    audient_data = data.model_dump()
     for item in audient_data["audience"]:
-        audient_collection.update_one(
-            {"_id": item["id"]},
-            {"$set": item},
-            upsert=True
-        )
+        audient_collection.update_one({"_id": item["id"]}, {"$set": item}, upsert=True)
     return {"Success": audient_data}
